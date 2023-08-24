742b8af61d5e85c2ee16ff766269bc8d00d84229
“mukkssii” <kapitanbukash@gmail.com>
2023-08-24 18:38:04 +0200

    did something

---
+++ app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title='First our app',
+    description='we are the champions',
+    version='0.0.1',
+    debug=True,
+)
+
+
+@app.get('/')
+@app.post('/')
+async def main_page() -> dict:
+    return {'greeting': 'HELLO MY FRIEND!!!!'}
+
+
+@app.get('/{user_name}')
+@app.get('/{user_name}/{user_nick}')
+async def user_page(user_name: str, user_nick: str = '', limit: int = 10, skip: int = 0) -> dict:
+    data = [i for i in range(1000)][skip:][:limit]
+
+    return {'user_name': user_name, 'user_nick': user_nick, 'data': data}
